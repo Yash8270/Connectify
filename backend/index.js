00751d87ee61a1429249f1aeaa4d0c4eb3871759
Yash8270 <yashlimbachiya900@gmail.com>
2025-12-07 23:36:45 +0530

    Frontend vercel link added

--- backend/index.js
+++ backend/index.js
@@ -19,14 +19,14 @@ if (result.error) {
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000","https://yash-limbachiya-connectify.vercel.app/"],
         methods: ['GET','POST','PATCH','DELETE','PUT'],
         credentials: true
     }
 })
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', "https://yash-limbachiya-connectify.vercel.app/"],
   credentials: true,
 }));
 
